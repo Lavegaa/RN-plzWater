553e69b46c76b970981b5e5d62a5bafd79d5fd74
Lavegaa <cksgh405@naver.com>
2020-02-01 11:33:02 +0900

    Add inputs like plant's name, water day. Add submit button that use context for create plant state.

--- src/components/Modal/ModalAdd/ModalAdd.tsx
+++ src/components/Modal/ModalAdd/ModalAdd.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Text,
+  TextInput,
   Modal,
   View,
   Alert,
@@ -11,10 +12,26 @@ import {
   useModalState,
   useModalDispatch
 } from "../../../contexts/ModalContext";
+import { usePlantDispatch } from "../../../contexts/PlantContext";
 
 export default function ModalAdd() {
   const modalState = useModalState();
-  const dispatch = useModalDispatch();
+  const modalDispatch = useModalDispatch();
+  const plantDispatch = usePlantDispatch();
+  const [name, setName] = useState("");
+  const [inputDay, setInputDay] = useState("");
+  const [day, setDay] = useState(0);
+  const handleInput = inputDay => {
+    setDay(parseInt(inputDay));
+    setInputDay(inputDay);
+  };
+  const handleSubmit = () => {
+    plantDispatch({ type: "CREATE", name: name, waterDay: day });
+    modalDispatch({ type: "TOGGLE_ADDMODAL" });
+    setDay(0);
+    setInputDay("");
+    setName("");
+  };
   return (
     <View style={styles.container}>
       <Modal
@@ -25,10 +42,30 @@ export default function ModalAdd() {
           Alert.alert("modal has been closed");
         }}
       >
-        <Text>안녕 모달모달</Text>
+        <Text>식물 등록</Text>
+        <Text>식물 이름</Text>
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="식물 이름을 입력해 주세요."
+          onChangeText={name => setName(name)}
+          value={name}
+        />
+        <Text>식물 물주는 날</Text>
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="식물 물주는 날을 입력해 주세요."
+          onChangeText={inputDay => handleInput(inputDay)}
+          value={inputDay}
+        />
+        <TouchableHighlight onPress={handleSubmit}>
+          <Text>등록</Text>
+        </TouchableHighlight>
+        <Text>
+          {name},{day}
+        </Text>
         <TouchableHighlight
           onPress={() => {
-            dispatch({ type: "TOGGLE_ADDMODAL" });
+            modalDispatch({ type: "TOGGLE_ADDMODAL" });
           }}
         >
           <Text>모달닫기</Text>
